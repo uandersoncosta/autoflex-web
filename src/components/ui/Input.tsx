4e67b9d86be16f6f8eca9e094baddf99c9f3dd1b
Uanderson Costa <uandersonferreiracosta@gmail.com>
2026-02-08 21:44:17 -0300

    feat: input hook

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,27 @@
+import React, { InputHTMLAttributes, forwardRef } from 'react';
+import './Input.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  helperText?: string;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, error, helperText, className = '', ...props }, ref) => {
+    return (
+      <div className="input-wrapper">
+        {label && <label className="input-label">{label}</label>}
+        <input
+          ref={ref}
+          className={`input ${error ? 'input-error' : ''} ${className}`}
+          {...props}
+        />
+        {error && <span className="input-error-text">{error}</span>}
+        {helperText && !error && <span className="input-helper-text">{helperText}</span>}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
